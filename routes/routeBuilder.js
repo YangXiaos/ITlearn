41a1be45ef2a18cf7b2c221a54275ed9a91ccdff
YangXiaos <178069857@qq.com>
2018-02-14 21:51:14 +0800

    添加路由路径，关于父级路径的设定

--- routes/routeBuilder.js
+++ routes/routeBuilder.js
@@ -7,7 +7,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var async = require('async');
 var CollectionsId = require('../models/collectionsIdSchema');
-
+var settings = require('../settings');
 
 mongoose.connect('mongodb://localhost/ITlearning');
 
@@ -53,32 +53,24 @@ function RouterBuilder(modelBuilder, routerOptions) {
     // 设置默认参数
     var defaultRouterOptions = {
         resourceUrl: "",
+        pidUrl: undefined,
         limit: 20,
 
         // 参数提取
         toMeta: {}, // 属性控制变量{name: String}
-
-        // 路由函数
+        // 钩子函数
         postFn: [],
         deleteFn: [],
         getFn: [],
         patchFn: [],
 
-        postSuccess: function (req, doc) {
-
-        },
-        deleteSuccess: function (req, doc) {
-
-        },
-        getSuccess: function (req, doc) {
-
-        },
-        patchSuccess: function (req, doc) {
-
-        },
-
+        postSuccess: function (req, doc) {},
+        deleteSuccess: function (req, doc) {},
+        getSuccess: function (req, doc) {},
+        patchSuccess: function (req, doc) {},
         extraRule: []
     };
+
     for (var key in defaultRouterOptions){
         if (!(key in routerOptions)){
             routerOptions[key] = defaultRouterOptions[key];
@@ -90,8 +82,13 @@ function RouterBuilder(modelBuilder, routerOptions) {
     this.model = modelBuilder.model;
     this.router = express.Router();
 
-    // 获取路径参数函数, query函数
-    this.getQueries = function (req, res, next) {
+    /**
+     * query 抽取函数
+     * @param req
+     * @param res
+     * @param next
+     */
+    this.getConditions = function (req, res, next) {
         req.metaKey = null;
         req.conditions = getConditions(req.query, this.fields);
         req.doc = getConditions(req.body, this.fields);
@@ -105,28 +102,14 @@ function RouterBuilder(modelBuilder, routerOptions) {
             conditions = Object.assign(req.params, req.conditions);
         }
         req.conditions = conditions;
-
         next();
     };
 
-    this.router.route("*").all(this.getQueries);
+    this.router.route("*").all(this.getConditions);
 
     for (var i = 0; i < routerOptions.extraRule.length; i++){
         var rule = routerOptions.extraRule[i];
-        switch (rule.method){
-            case "post" || "POST":
-                routerBuilder.router.post(rule.url, rule.fn);
-                break;
-            case "DELETE" || "delete":
-                routerBuilder.router.delete(rule.url, rule.fn);
-                break;
-            case "get":
-                routerBuilder.router.get(rule.url, rule.fn);
-                break;
-            case "PATCH" || "patch":
-                routerBuilder.router.patch(rule.url, rule.fn);
-                break;
-        }
+        routerBuilder.router[rule.method](rule.url, rule.fn);
     }
 
     this.router.route(routerOptions.resourceUrl)
@@ -134,19 +117,20 @@ function RouterBuilder(modelBuilder, routerOptions) {
             routerBuilder.model.create(req.doc, function (err, doc) {
                 // 异常操作
                 if (err){
-
+                    res.status(404);
+                    res.json({error: err});
                 } else {
                     routerOptions.postSuccess(req, doc);
+                    res.json(doc);
                 }
-
-                res.json(doc);
             });
         })
         .delete(routerOptions.deleteFn, function (req, res) {
             routerBuilder.model.remove(req.conditions, function (err, result) {
                 // 异常操作
                 if (err){
-
+                    res.status(404);
+                    res.json({error: err});
                 } else {
                     routerOptions.deleteSuccess(req, result);
                     res.json(result);
@@ -167,7 +151,8 @@ function RouterBuilder(modelBuilder, routerOptions) {
             routerBuilder.model.find(req.conditions, req.metaKey, options, function (err, docs) {
                 // 异常操作
                 if (err){
-
+                    res.status(404);
+                    res.json({error: err});
                 } else {
                     routerOptions.getSuccess(req, docs);
                     res.json(docs);
@@ -177,17 +162,24 @@ function RouterBuilder(modelBuilder, routerOptions) {
         })
         .patch(routerOptions.patchFn, function (req, res) {
 
-            var operation = req.query.operation;
-            var toMeta = req.query.toMeta;
-            var val = toMeta? routerOptions[toMeta](req.query.value): undefined;
+            var operation = req.query["operation"];  // 其他操作方法　类似$push
+            var toMeta = req.query.toMeta;  // 修改的对象属性,
+            var value = toMeta? req.body[toMeta] : undefined;  // 修改的对象的操作的值
 
-            // json修改
-            var update = operation? {operation: {toMeta: val}}: {$set: req.body};
+            // update 设定
+            var update = {};
+            if (operation){
+                update[operation] = {};
+                update[operation][toMeta] = value;
+            } else {
+                update = {$set: req.doc};
+            }
 
             routerBuilder.model.update(req.conditions, update, function (err, docs) {
                 // 异常操作
                 if (err){
-
+                    res.status(404);
+                    res.json({error: err});
                 } else {
                     routerOptions.getSuccess(req, docs);
                     res.json(docs);
@@ -197,8 +189,13 @@ function RouterBuilder(modelBuilder, routerOptions) {
         });
 
     this.getRouterPath = function () {
-        return "/" + this.collectionName;
-    }
+        if(routerOptions.pidUrl) {
+            return "/" + settings.version + "/" + routerOptions.pidUrl;
+        } else {
+            return "/" + settings.version + "/" + this.collectionName;
+        }
+    };
+
 }
 
 module.exports = RouterBuilder;
