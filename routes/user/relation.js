f06ec4794d43f923b0cc3d3cbf14d66e5d084096
YangXiaos <178069857@qq.com>
2018-04-05 16:42:42 +0800

    添加关注用户toggle 函数

--- routes/user/relation.js
+++ routes/user/relation.js
@@ -10,6 +10,25 @@ var commonFn = require('../../routes/commonFn');
 var Relation = require('../../models/user/relation').model;
 
 
+// 关注钩子函数
+var follow = function (req, res, next) {
+    var condition = {
+        user: req.query.user,
+        follower: req.query.follower
+    };
+    Relation.find(condition, function (err, data) {
+        if (data.length === 1) {
+            Relation.deleteOne(condition).then(function (err, result) {
+                res.send({status: 0, message: "取消关注成功", result: result});
+            });
+        } else {
+            Relation.create(condition, function (err, data) {
+                res.json({status: 0, message: "关注成功", data: data._doc});
+            });
+        }
+    });
+};
+
 
 module.exports = new RouteBuilder(
     mBuilder,
@@ -30,22 +49,12 @@ module.exports = new RouteBuilder(
         extraRule: [
             {
                 method: "get",
-                url: "/count",
-                fn: function (req, res, next) {
-                    Relation.count(req.conditions, function (err, count) {
-                        if (!err){
-                            // 返回请求结果
-                            // res.json();
-                        } else {
-                            // 返回请求结果
-
-                        }
-                    })
-                }
+                url: "/follow/",
+                fn: follow
             }
         ],
 
-
+        // 填充关联字段
         populate: "user follower",
         pidUrl: "/users",
         resourceUrl: "/:user/relations",
