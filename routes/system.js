25d362d88ca8096be6aa8c21e1be3b02696cfeea
YangXiaos <178069857@qq.com>
2018-04-29 12:11:15 +0800

    修改了没有显示用户的bug

--- routes/system.js
+++ routes/system.js
@@ -12,12 +12,12 @@ var router = express.Router();
 router.all("/email", function (req, res, next) {
     sendEmail(req.body.email, function (err, info) {
         console.log(err);
-        if (err) {
-            res.json({status: 1, message: "发送失败"})
-        } else if (err === "no user"){
-            res.json({status: 1, message: info})
+        if (err === "no user") {
+            res.json({status: 1, message: info});
+        } else if (err){
+            res.json({status: 1, message: "发送失败"});
         } else {
-            res.json({status: 0, message: info})
+            res.json({status: 0, message: info});0
         }
     });
 });
